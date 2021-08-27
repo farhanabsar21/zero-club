3f187107484d8010c3bb93c37332ddd88f3dd437
Farhan21 <farhanabsar21@gmail.com>
2021-08-27 12:48:05 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ClubProfile from "./Components/Sub-components/ClubProfile/ClubProfile";
 import SignIn from "./Components/Sub-components/Sign-in/SignIn";
 
 function App() {
   return (
     <div className="App">
-        <SignIn/>
+        {/* <SignIn/> */}
+        <ClubProfile/>
     </div>
   );
 }
